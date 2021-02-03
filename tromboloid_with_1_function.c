3f74d8b86438f66d1832a80c2706d37633fca139
Mugdha KG <69378916+Mugdhakg@users.noreply.github.com>
2021-02-03 11:50:28 +0530

    Update tromboloid_with_1_function.c

--- tromboloid_with_1_function.c
+++ tromboloid_with_1_function.c
@@ -2,9 +2,9 @@
 #include<stdio.h>
 int main()
 {
-    float h,d,b, volume;
-    printf("Enter the height, depth and breadth of a tromboid = ");
-    scanf("%f%f%f", &h,&d&b);
-    volume = 1/3((h*d*b)+(d/b));
-    printf("The volume of the tromboid is = %f," volume);
+    float h,d,b,volume;
+    printf("Enter the height, depth and breadth of a tromboloid = ");
+    scanf("%f%f%f",&h,&d,&b);
+    volume = ((h*d*b)+(d/b))/3;
+    printf("The volume of the tromboloid is = %lf", volume);
 }
