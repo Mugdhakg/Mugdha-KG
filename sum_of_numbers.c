c97bb6c1709636602144dfa7603d3f3a09bbbf4d
Mugdha KG <69378916+Mugdhakg@users.noreply.github.com>
2021-02-08 14:51:39 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,1 +1,18 @@
 //Write a program to find the sum of n different numbers using 4 functions
+#include<stdio.h>
+int main ()
+{
+float n, i, sum;
+printf("Enter the number of numbers = ");
+scanf("%10.2f",&n);
+
+printf("Enter %10.2f numbers = ",n);
+
+for(i=1; i<=n; i++)
+{
+scanf("%10.2f", &i);
+sum = sum++
+}
+printf("Sum of the entered %10.2f numbers = %10.2f", n,sum);
+return 0;
+}
