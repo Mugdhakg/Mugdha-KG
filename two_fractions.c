1ca1320171cedf78242a935539ec745b8381b581
Mugdha KG <69378916+Mugdhakg@users.noreply.github.com>
2021-03-06 08:01:56 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -7,18 +7,18 @@ typedef struct
 	int deno;
 }
 Fract;
-Fract sum(Fract,Fract)
-; int main()
+Fract sum(Fract,Fract);
+int main()
 {
 	int num1,deno1,num2,deno2;
-	printf("Enter fraction 1: number denominator:");
-  scanf("%d%d", &num1,&deno1);
-  printf("Enter fraction 2: numerator denominator:");
-  scanf("%d%d",&num2,&deno2);
-  Fract f1 = {num1,deno1};
-  Fract f2 = {num2,deno2};
-  Fract result = sum(f1,f2);
-  printf("Result = %d/%d", result.num,result.deno);
-  getch();
-  return 0;
+	printf("Enter fraction 1: numerator and denominator:");
+  	scanf("%d%d", &num1,&deno1);
+  	printf("Enter fraction 2: numerator and denominator:");
+  	scanf("%d%d",&num2,&deno2);
+  	Fract f1 = {num1,deno1};
+  	Fract f2 = {num2,deno2};
+  	Fract result = sum(f1,f2);
+  	printf("Result = %d/%d", result.num,result.deno);
+  	getch();
+  	return 0;
 }
