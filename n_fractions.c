6bcbee939d46125dee024d5f769b59333c0ab3b6
Mugdha KG <69378916+Mugdhakg@users.noreply.github.com>
2021-03-06 08:18:42 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -2,23 +2,23 @@
 #include<stdio.h>
 int main()
 {
-int a,b,c,d,x,y,i,gcd;
-printf("Enter the numerator for the first fraction = ");
-scanf("%d", &a);
-printf("Enter the denominator for the first fraction = ");
-scanf("%d", &b);
-printf("Enter the numerator for second fraction = ");
-scanf("%d",c);
-printf("Enter the denominator for second fraction = ");
-scanf("%d", &d);
-x = (a*d)+(b*c);
-y = b*d;
-for(i=1; i<=x <<x && i<=y; ++i)
-{
-if(x%i==0 && y%i==0)
-gcd = i;
-}
-printf("The added fraction is %d/%d",x/gcd,y/gcd);
-printf("\n");
-return 0;
+    int a,b,c,d,x,y,i,gcd;
+    printf("Enter the numerator for the first fraction = ");
+    scanf("%d", &a);
+    printf("Enter the denominator for the first fraction = ");
+    scanf("%d", &b);
+    printf("Enter the numerator for second fraction = ");
+    scanf("%d",c);
+    printf("Enter the denominator for second fraction = ");
+    scanf("%d", &d);
+    x = (a*d)+(b*c);
+    y = b*d;
+    for(i=1; i<=x && i<=y; i++)
+    {
+        if(x%i==0 && y%i==0)
+        gcd = i;
+    }
+    printf("The added fraction is %d/%d",x/gcd,y/gcd);
+    printf("\n");
+    return 0;
 } 
